d394affd584643ac91caef08a9cff2c10f89cd39
thakursom <58730677+thakursom@users.noreply.github.com>
2025-11-17 17:23:23 +0530

    implement sun label summary page

---
+++ src/pages/SuperAdmin/SubLabelSummary.jsx
@@ -0,0 +1,7 @@
+import SubLabelSummaryComponent from "../../components/SuperAdmin/SubLabelSummaryComponent"
+
+function SubLabelSummary() {
+    return <SubLabelSummaryComponent />;
+}
+
+export default SubLabelSummary
